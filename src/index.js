b5ba4fa1054c4f5b474681ced08494035fe51774
Jannery Briceño <jannerymai@gmail.com>
2023-03-28 10:45:27 -0500

    Add queries to navbar and work btn for min-width 1024px

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// const projects = document.querySelectorAll('.project');
+
+// projects.forEach(project => {
+
+//   const signs = Array.from(project.children).filter(e => e.localName == 'p').map(e => e.children).forEach(sign => console.log(sign.style.visibily == 'hidden'))
+
+//   project.addEventListener('mouseover', () => {
+    
+//   })
+// })
+
+const btnProject =document.getElementById('button-show-projects');
+const btnIcon = document.getElementById('btn--icon');
+
+btnProject.addEventListener('mouseover', () => {
+  btnIcon.style.transform = 'translate(4px)'
+})
+
+btnProject.addEventListener('mouseout', () => {
+  btnIcon.style.transform = 'translate(-4px)'
+})
